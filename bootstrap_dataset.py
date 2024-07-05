3919adfaad50cb2dad2bf32747cda9c612baba78
Bonoldie <erribono01@gmail.com>
2024-07-05 16:31:14 +0200

    cleanup and dataset bootstrap script

---
+++ bootstrap_dataset.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+import requests
+from pathlib import Path
+
+def downloadFromUrl(url, filename, ds_dir = ".ds", force = False):
+    ds_dir = os.getcwd() + f"/{ds_dir}"
+    ds_path = os.path.join(ds_dir, filename)
+
+    if os.path.exists(ds_path) and not force:
+        print('Dataset file available')
+        return ds_path
+    
+    
+    if(os.path.exists(ds_dir) == False):
+        os.makedirs(ds_dir)
+    
+    print('Dataset is downloading...please wait')
+    
+    req = requests.get(url, allow_redirects=True)
+
+    print(f"Dataset downloaded, available at {ds_dir}")
+
+    open(ds_path, 'wb').write(req.content)
+
+    return ds_path
+
+ds_path = downloadFromUrl("https://www.kaggle.com/datasets/msarmi9/food101tiny?resource=download", "dataset.zip", ".ds.tiny.test")
